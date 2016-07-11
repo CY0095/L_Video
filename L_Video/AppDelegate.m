5c66a49b3afc73245b6340fed8ffe71bb61ec514
cy <1158524018@qq.com>
2016-07-11 14:49:47 +0800

    加代码

--- L_Video/AppDelegate.m
+++ L_Video/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // hello word
     return YES;
 }
 
